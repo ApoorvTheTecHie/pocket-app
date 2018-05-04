856f73a1e840b930d2094eab2d6d98069727fb89
Apoorv <apoorv.techindustan@gmail.com>
2018-05-04 18:30:12 +0530

    working on login page

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -26,7 +26,17 @@ export default class Login extends React.Component{
             }
         }
     }
+
+    /* onChange = (key,value) => {
+        let { user } = this.state;
+        user[key] = value;
+
+        this.setState({ user });
+        
+    } */
+
     render(){
+        let { user } = this.state;
         return(
             <Grid style={{ padding: '0' }}>
                 <Grid style={{ padding: '0' }}>
@@ -59,26 +69,42 @@ export default class Login extends React.Component{
                     </Navbar>
                 </Grid>
                     <Col>
-                    <Col lg={6} md={6} sx={12} sm={12}>
+                    <Col lg={6} md={6} xs={12} sm={12}>
                         <Image src="https://getpocket.com/i/v4/signup_graphic@1x.jpg" />
                     </Col>
-                    <Col lg={6} md={6} sx={12} sm={12}>
-                        <Well style={{ margin : '1em auto'}}>
+                    <Col lg={6} md={6} xs={12} sm={12}>
+                        <Well style={{  display : 'flex', flexDirection : 'column', alignItems : 'center'}}>
                            <h2 style={{ textAlign : 'center', fontWeight : '700'}}>Log In</h2>
                            <Button>Log IN with google</Button> <br />
                             <Button>Log IN with facebook</Button>
                             <Form horizontal>
                                 <FormGroup controlId="formHorizontalEmail">
                                     <Col sm={10}>
-                                        <FormControl type="email" placeholder="Email" />
+                                        <FormControl 
+                                            value={user.userName}
+                                            
+                                            type="email" 
+                                            placeholder="Email" 
+                                        />
                                     </Col>
                                 </FormGroup>
                                 <FormGroup controlId="formHorizontalPassword">
                                     <Col sm={10}>
-                                        <FormControl type="password" placeholder="Password" />
+                                        <FormControl 
+                                            type="password" 
+                                            placeholder="Password" 
+                                        />
                                     </Col>
-                                </FormGroup>
+                                </FormGroup>   
                             </Form>    
+                            <p>OR</p>
+                            <Button style={{ width : '80%'}} bsStyle="danger">Login</Button>
+                            <Link to="/">
+                                <p style={{textAlign : 'center'}}>Forgot your username or password</p>
+                            </Link>
+                            <Link to="/">
+                                <p style={{ textAlign: 'center' }}>Sign up now</p>
+                            </Link>
                         </Well>
                     </Col>
                 </Col>
